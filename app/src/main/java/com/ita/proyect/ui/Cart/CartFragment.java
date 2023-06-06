1acf82a8c9aae61da4dbc9f3d930a273275c3a1f
NobleRainbow58 <lopzar9188@gmail.com>
2023-06-06 17:00:59 -0600

    Login n QR code

--- app/src/main/java/com/ita/proyect/ui/Cart/CartFragment.java
+++ app/src/main/java/com/ita/proyect/ui/Cart/CartFragment.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.content.Intent;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.ita.proyect.ActivityQR;
 import com.ita.proyect.databinding.FragmentCartBinding;
 
 
@@ -32,6 +34,17 @@ public class CartFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public void iniciarActividadB() {
+        // Crea un Intent para iniciar la actividad B
+        Intent intent = new Intent(getActivity(), ActivityQR.class);
+
+        // Opcional: Puedes pasar información adicional a la nueva actividad utilizando putExtra
+        //intent.putExtra("clave", valor);
+
+        // Inicia la actividad B
+        startActivity(intent);
     }
 
 }
