6728f75732c7965078e53cbc56ec9d9ba12e0838
RicardoGP12 <20150411@aguascalientes.tecnm.mx>
2023-06-06 20:32:51 -0600

    registrar

--- app/src/main/java/com/ita/proyect/RegitrarActivity.java
+++ app/src/main/java/com/ita/proyect/RegitrarActivity.java
@@ -23,8 +23,8 @@ import java.util.Map;
 
 public class RegitrarActivity extends AppCompatActivity {
 
-    Button btn_regis;
-    EditText name,email, password;
+    Button btn_register;
+    EditText name, email, password;
     FirebaseFirestore mFirestore;
     FirebaseAuth mAuth;
 
@@ -32,47 +32,53 @@ public class RegitrarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regitrar);
+        this.setTitle("Registro");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         mFirestore = FirebaseFirestore.getInstance();
         mAuth = FirebaseAuth.getInstance();
 
-        name= findViewById(R.id.name);
+        name = findViewById(R.id.name_r);
         email = findViewById(R.id.email_r);
         password = findViewById(R.id.password_r);
-        btn_regis = findViewById(R.id.btn_register);
+        btn_register = findViewById(R.id.btn_register);
 
-        btn_regis.setOnClickListener(new View.OnClickListener() {
+        btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 String nameUser = name.getText().toString().trim();
                 String emailUser = email.getText().toString().trim();
                 String passUser = password.getText().toString().trim();
 
-                if (nameUser.isEmpty() && emailUser.isEmpty() && passUser.isEmpty()) {
+                if (nameUser.isEmpty() && emailUser.isEmpty() && passUser.isEmpty()){
                     Toast.makeText(RegitrarActivity.this, "Complete los datos", Toast.LENGTH_SHORT).show();
-                }else {
-                    registerUser(nameUser,emailUser,passUser);
+                }else{
+                    registerUser(nameUser, emailUser, passUser);
                 }
             }
         });
     }
 
     private void registerUser(String nameUser, String emailUser, String passUser) {
-        mAuth.createUserWithEmailAndPassword(emailUser,passUser).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        mAuth.createUserWithEmailAndPassword(emailUser, passUser).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                String id = mAuth.getCurrentUser().getUid();
-                Map<String,Object> map = new HashMap<>();
-                map.put("id",id);
-                map.put("name",nameUser);
-                map.put("email",emailUser);
-                map.put("password",passUser);
+                String id;
+
+                //id = mAuth.getUid();
+                id = mAuth.getCurrentUser().getUid();
+                Map<String, Object> map = new HashMap<>();
+                map.put("id", id);
+                map.put("name", nameUser);
+                map.put("email", emailUser);
+                map.put("password", passUser);
 
                 mFirestore.collection("user").document(id).set(map).addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void unused) {
                         finish();
-                        startActivity(new Intent(RegitrarActivity.this,MainActivity.class));
-                        Toast.makeText(RegitrarActivity.this, "Usario agregado", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(RegitrarActivity.this, MainActivity.class));
+                        Toast.makeText(RegitrarActivity.this, "Usuario registrado con éxito", Toast.LENGTH_SHORT).show();
                     }
                 }).addOnFailureListener(new OnFailureListener() {
                     @Override
@@ -87,5 +93,11 @@ public class RegitrarActivity extends AppCompatActivity {
                 Toast.makeText(RegitrarActivity.this, "Error al registrar", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return false;
     }
 }
